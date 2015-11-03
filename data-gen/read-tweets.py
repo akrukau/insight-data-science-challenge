5ec0a06e39c985181feddcd8f1ec03cfe28e2e32
Aliaksandr Krukau <akrukau@silo.soic.indiana.edu>
2015-11-02 19:07:44 -0500

    Unworking version with object hooks

--- data-gen/read-tweets.py
+++ data-gen/read-tweets.py
@@ -9,11 +9,16 @@ with open('tweets.txt') as input_file:
     count = 0
     for line in input_file:
         tweet = json.loads(line)
+        tags = []
         if "entities" in tweet and "hashtags" in tweet["entities"]:
         #if "entities" in tweet:
             #data.append(tweet["entities"]["hashtags"])
             tags.append(tweet['entities']['hashtags'])
             count +=1 
+            tag_json_array = tweet['entities']['hashtags']
+            for tag_json in tag_json_array:
+                if "text" in tag_json:
+                    tags.append(tag_json["text"])
 
     #print "Count",count
 
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,47 @@
+#!/usr/bin/python
+#
 # example of program that calculates the number of tweets cleaned
+#
+import json
+from pprint import pprint
+
+def _decode_list(data):
+    rv = []
+    for item in data:
+        #if isinstance(item, unicode):
+        #    item = item.encode('utf-8')
+        if isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        #if isinstance(key, unicode):
+        #    key = key.encode('utf-8')
+        #if isinstance(value, unicode):
+        #    value = value.encode('utf-8')
+        if isinstance(value, list):
+            value = _decode_list(value)
+        elif isinstance(value, dict):
+            value = _decode_dict(value)
+        rv[key] = value
+    return rv
+
+with open('../data-gen/tweets.txt', 'rb') as input_file:
+    for line in input_file:
+        tweet = json.loads(line, object_hook = _decode_dict)
+        cleaned_tweet = ""
+
+        if "text" in tweet:
+            #cleaned_tweet += ''.join([ char for char in tweet["text"] if ord(char) <= 255 ])  
+            cleaned_text = tweet["text"]
+            #cleaned_text = cleaned_text.encode('unicode-escape') 
+            cleaned_tweet += cleaned_text
+
+        print cleaned_tweet       
+
+
