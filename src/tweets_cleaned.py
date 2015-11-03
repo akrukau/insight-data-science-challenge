f0690ba9e58b6fa050db7b7aaa8741117d38f907
Aliaksandr Krukau <akrukau@silo.soic.indiana.edu>
2015-11-02 20:42:53 -0500

    First version that outputs timestamp, and uses regex for cleaning.

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -3,45 +3,33 @@
 # example of program that calculates the number of tweets cleaned
 #
 import json
+import re
 from pprint import pprint
 
-def _decode_list(data):
-    rv = []
-    for item in data:
-        #if isinstance(item, unicode):
-        #    item = item.encode('utf-8')
-        if isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        rv.append(item)
-    return rv
-
-def _decode_dict(data):
-    rv = {}
-    for key, value in data.iteritems():
-        #if isinstance(key, unicode):
-        #    key = key.encode('utf-8')
-        #if isinstance(value, unicode):
-        #    value = value.encode('utf-8')
-        if isinstance(value, list):
-            value = _decode_list(value)
-        elif isinstance(value, dict):
-            value = _decode_dict(value)
-        rv[key] = value
-    return rv
-
 with open('../data-gen/tweets.txt', 'rb') as input_file:
+    #p = re.compile(r'(\"text:\"'))
+    #re.findall(p, line)
+    count_tweets_unicode = 0
     for line in input_file:
-        tweet = json.loads(line, object_hook = _decode_dict)
-        cleaned_tweet = ""
+        # Remove all \uxxxx sequences
+        (line, deletions) = re.subn(r"\\u[0-9a-f]{4}", "", line) 
+        if deletions > 0:
+            count_tweets_unicode += 1
 
+        tweet = json.loads(line)
+        cleaned_text = ""
         if "text" in tweet:
             #cleaned_tweet += ''.join([ char for char in tweet["text"] if ord(char) <= 255 ])  
             cleaned_text = tweet["text"]
             #cleaned_text = cleaned_text.encode('unicode-escape') 
-            cleaned_tweet += cleaned_text
 
-        print cleaned_tweet       
+        timestamp = ""
+        if "created_at" in tweet:
+            timestamp = tweet["created_at"] 
 
+        cleaned_tweet = cleaned_text + " (timestamp: " + timestamp + ")" 
+        print cleaned_tweet
+
+    print
+    print count_tweets_unicode, "tweets contained unicode."
 
