89dbb2dd2b35ba7d13bce978c1d9837c2046a7d2
Aliaksandr Krukau <akrukau@silo.soic.indiana.edu>
2015-11-02 22:32:48 -0500

    First working version that removes non-ascii characters and new lines.

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python
 #
 # example of program that calculates the number of tweets cleaned
+# Created by Aliaksandr Krukau, 11/2/2015
 #
 import json
 import re
-from pprint import pprint
 
 with open('../data-gen/tweets.txt', 'rb') as input_file:
-    #p = re.compile(r'(\"text:\"'))
-    #re.findall(p, line)
     count_tweets_unicode = 0
+
     for line in input_file:
-        # Remove all \uxxxx sequences
-        (line, deletions) = re.subn(r"\\u[0-9a-f]{4}", "", line) 
-        if deletions > 0:
-            count_tweets_unicode += 1
-
         tweet = json.loads(line)
-        cleaned_text = ""
         if "text" in tweet:
-            #cleaned_tweet += ''.join([ char for char in tweet["text"] if ord(char) <= 255 ])  
-            cleaned_text = tweet["text"]
-            #cleaned_text = cleaned_text.encode('unicode-escape') 
+            text = tweet["text"]
+            
+            #Remove all non-ascii characters that correspond to \uxxxx sequences
+            (cleaned_text, unicode_deletions) = re.subn(r'[^\x00-\x7F]','', text)
+            #Remove all characters represented by two-character escape sequence
+            (cleaned_text, escape_deletions) = re.subn(r'[\"\\\b\f\n\r\t]','', cleaned_text)
+            if unicode_deletions > 0:
+                count_tweets_unicode += 1
 
-        timestamp = ""
-        if "created_at" in tweet:
-            timestamp = tweet["created_at"] 
+            timestamp = ""
+            if "created_at" in tweet:
+                timestamp = tweet["created_at"] 
 
-        cleaned_tweet = cleaned_text + " (timestamp: " + timestamp + ")" 
-        print cleaned_tweet
+            cleaned_tweet = cleaned_text + " (timestamp: " + timestamp + ")" 
+            print cleaned_tweet
 
     print
     print count_tweets_unicode, "tweets contained unicode."
