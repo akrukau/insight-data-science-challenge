d4c0639de5374de1634d40419c92200a7f3da4ec
Aliaksandr Krukau <akrukau@silo.soic.indiana.edu>
2015-11-03 21:03:01 -0500

    Changed from priority queue to heap.

--- src/average_degree.py
+++ src/average_degree.py
@@ -2,16 +2,16 @@
 #
 # example of program that calculates the average degree of hashtags
 #
-# Created by Aliaksandr Krukau, 11/2/2015 
+# Created by Aliaksandr Krukau, 11/3/2015 
 #
 # Requires Python 2.7 because of "with" statement for two files
 #
-import Queue
 import json
 import sys
 import re
 from datetime import datetime, timedelta
 from email.utils import parsedate_tz
+from heapq import heappush, heappop
 
 def clean(text):
     #Remove all non-ascii characters that correspond to \uxxxx sequences
@@ -26,10 +26,12 @@ def clean(text):
 def extract_time(timestamp_string):
     time_tuple = parsedate_tz(timestamp_string.strip())
     raw_time = datetime(*time_tuple[:6])
-    return raw_time - timedelta(seconds = time_tuple[-1])
+    timezone_correction = timedelta(seconds = time_tuple[-1]) 
+    return raw_time - timezone_correction
 
-with open('../data-gen/short-tweets.txt', 'rb') as input_file:
-    heap = Queue.PriorityQueue()
+#with open('../data-gen/short-tweets.txt', 'rb') as input_file:
+with open('../data-gen/tweets.txt', 'rb') as input_file:
+    heap = []
     for line in input_file:
         try:
             tweet = json.loads(line)
@@ -39,16 +41,17 @@ with open('../data-gen/short-tweets.txt', 'rb') as input_file:
             if "created_at" in tweet:
                 timestamp_string = tweet["created_at"]
                 timestamp = extract_time(timestamp_string)
-                print "Timestamp",timestamp
-            tweet_entry = (timestamp, tags[0])   
-            heap.put(tweet_entry)               
+                #print timestamp
+                tweet_entry = (timestamp, tags[0])   
+                heappush(heap, tweet_entry)
+
 
         except IndexError:
             sys.stderr.write("We suspect that the following line is not valid JSON\n")
             sys.stderr.write(line)
 
-    while not heap.empty():        
-        tweet = heap.get()
-        print tweet
+    while queue:        
+        tweet = heappop(heap)
+        #print tweet
             
 
